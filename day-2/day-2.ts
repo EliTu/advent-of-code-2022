5a924bb6fba957ecca474d149ec40edeecf7d1a5
Eliad Tuksher <eliad91@gmail.com>
2022-12-04 22:16:50 +0200

    day 2 part-1 code improvements

--- day-2/day-2.ts
+++ day-2/day-2.ts
@@ -4,51 +4,57 @@
 
 import { readFileSync } from 'fs';
 
+/**
+ * Part 1 - What would your total score be if everything goes exactly according to your strategy guide?
+ * Answer: 11873
+ */
+
 type MyShape = 'X' | 'Y' | 'Z';
 type OpponentShape = 'A' | 'B' | 'C';
 interface StrategyData {
 	points: number;
-	winsAgainst: OpponentShape;
-	losesTo: OpponentShape;
+	winsAgainstShape: OpponentShape;
+	losesToShape: OpponentShape;
 }
 
 const strategyDataGuide: Record<MyShape, StrategyData> = {
 	X: {
 		points: 1,
-		winsAgainst: 'C',
-		losesTo: 'B',
+		winsAgainstShape: 'C',
+		losesToShape: 'B',
 	},
 	Y: {
 		points: 2,
-		winsAgainst: 'A',
-		losesTo: 'C',
+		winsAgainstShape: 'A',
+		losesToShape: 'C',
 	},
 	Z: {
 		points: 3,
-		winsAgainst: 'B',
-		losesTo: 'A',
+		winsAgainstShape: 'B',
+		losesToShape: 'A',
 	},
 };
 
 const inputText = readFileSync('./input.txt', 'utf-8');
 const splitInput = inputText.split('\n');
 
-const totalPoints = splitInput.reduce((total, round) => {
-	const [opponentShape, myShape] = round.split(' ');
-	const { losesTo, points, winsAgainst } = strategyDataGuide[myShape as MyShape];
+const totalPoints = splitInput.reduce((total, roundInfo) => {
+	const [opponentShape, myShape] = roundInfo.split(' ');
+	const { losesToShape, points, winsAgainstShape } = strategyDataGuide[myShape as MyShape];
 
 	let roundPoints = points;
 
 	// it's a draw
-	if (opponentShape !== losesTo && opponentShape !== winsAgainst) {
+	if (opponentShape !== losesToShape && opponentShape !== winsAgainstShape) {
 		roundPoints += 3;
 	}
 	// it's a win
-	if (opponentShape === winsAgainst) {
+	if (opponentShape === winsAgainstShape) {
 		roundPoints += 6;
 	}
 	// in case of lose, nothing changes as it is 0 points
 
 	return total + roundPoints;
 }, 0);
+
 console.log(totalPoints); // 11873
