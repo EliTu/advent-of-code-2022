0d3532b696603c94d55cd02476f2a862418587b3
Eliad Tuksher <eliad91@gmail.com>
2022-12-04 23:12:37 +0200

    Solve day 2 part 2

--- day-2/day-2.ts
+++ day-2/day-2.ts
@@ -42,7 +42,7 @@ const totalPoints = splitInput.reduce((total, roundInfo) => {
 	const [opponentShape, myShape] = roundInfo.split(' ');
 	const { losesToShape, points, winsAgainstShape } = strategyDataGuide[myShape as MyShape];
 
-	let roundPoints = points;
+	let roundPoints: number = points;
 
 	// it's a draw
 	if (opponentShape !== losesToShape && opponentShape !== winsAgainstShape) {
@@ -58,3 +58,48 @@ const totalPoints = splitInput.reduce((total, roundInfo) => {
 }, 0);
 
 console.log(totalPoints); // 11873
+
+/**
+ * Part 2 - Following the Elf's instructions for the second column, what would your total score be if everything goes exactly according to your strategy guide?
+ * Answer: 12014
+ */
+
+// Rock - 1 point
+// Paper - 2 points
+// Scissors - 3 points
+
+// Lose - 0 points
+// Draw - 3 points
+// Win - 6 points
+
+type StrategyToPointsMap = Record<MyShape, number>;
+
+const updatedStrategyGuide: Record<OpponentShape, StrategyToPointsMap> = {
+	// Opponent chooses rock
+	A: {
+		X: 3, // lose - pick scissors
+		Y: 4, // draw - pick rock
+		Z: 8, // win - pick paper
+	},
+	// Opponent chooses paper
+	B: {
+		X: 1, // lose - choose rock
+		Y: 5, // draw - pick paper
+		Z: 9, // win - pick scissors
+	},
+	// Opponent chooses scissors
+	C: {
+		X: 2, // lose - choose paper
+		Y: 6, // draw - choose scissors
+		Z: 7, // win - choose rock
+	},
+};
+
+const totalPoints2 = splitInput.reduce((total, roundInfo) => {
+	const [opponentShape, myStrategy] = roundInfo.split(' ');
+	const roundPoints = updatedStrategyGuide[opponentShape as OpponentShape][myStrategy as MyShape];
+
+	return total + roundPoints;
+}, 0);
+
+console.log(totalPoints2); // 12014
