b99b2f20ff14e7b31dc622829cc444988bab16d0
Eliad Tuksher <eliad91@gmail.com>
2022-12-04 18:54:00 +0200

    AOC - day 1

---
+++ day-1/day-1.js
@@ -0,0 +1,25 @@
+"use strict";
+/**
+ * Link: https://adventofcode.com/2022/day/1
+ */
+/**
+ * Part 1 - Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
+const inputText = (0, fs_1.readFileSync)('./input.txt', 'utf-8');
+const splitInput = inputText.split('\n\n');
+const totalCaloriesPerElf = splitInput.reduce((acc, curr) => {
+    const splitSums = curr.split('\n');
+    const totalCalories = splitSums.reduce((total, current) => total + Number(current), 0);
+    return [...acc, totalCalories];
+}, []);
+const maxCalories = Math.max(...totalCaloriesPerElf);
+console.log(maxCalories); // 67450
+/**
+ * Part 2 - Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
+ */
+const sortedTotalCalories = totalCaloriesPerElf.sort((a, b) => b - a);
+const topElvesWithMostCalories = sortedTotalCalories.slice(0, 3);
+const totalTopElvesCalories = topElvesWithMostCalories.reduce((total, curr) => total + curr);
+console.log(totalTopElvesCalories);
