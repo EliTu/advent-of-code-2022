1e7b57e25c9ed06788fe5579be57b01b224d2d19
Eliad Tuksher <eliad91@gmail.com>
2022-12-06 20:36:00 +0200

    Solve day 4 part 2

--- day-4/day-4.ts
+++ day-4/day-4.ts
@@ -9,6 +9,7 @@ const sectionsData = inputText.split('\n');
 
 /**
  * Part 1 - In how many assignment pairs does one range fully contain the other?
+ * Answer - 471
  */
 
 const numberOfOverlaps = sectionsData.reduce((total, sections) => {
@@ -26,4 +27,27 @@ const numberOfOverlaps = sectionsData.reduce((total, sections) => {
 
 	return total;
 }, 0);
-console.log(numberOfOverlaps);
+
+console.log(numberOfOverlaps); // 471
+
+/**
+ * Part 2 - In how many assignment pairs do the ranges overlap?
+ * Answer - 888
+ */
+
+const numberOfOverlaps2 = sectionsData.reduce((total, sections) => {
+	const [firstSection, secondSection] = sections.split(',') as [string, string];
+
+	const [firstSectionLowerBound, firstSectionUpperBound] = firstSection.split('-').map(Number);
+	const [secondSectionLowerBound, secondSectionUpperBound] = secondSection.split('-').map(Number);
+
+	const isNoBoundOverlap =
+		firstSectionUpperBound < secondSectionLowerBound || firstSectionLowerBound > secondSectionUpperBound;
+
+	if (isNoBoundOverlap) {
+		return total;
+	}
+	return total + 1;
+}, 0);
+
+console.log(numberOfOverlaps2); // 888
