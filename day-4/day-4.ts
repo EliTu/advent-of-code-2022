96b84aac93b78eb73e08dd392b69b0a9ea01fd0c
Eliad Tuksher <eliad91@gmail.com>
2022-12-06 20:55:01 +0200

    Day 4 - improve code

--- day-4/day-4.ts
+++ day-4/day-4.ts
@@ -7,16 +7,29 @@ import { readFileSync } from 'fs';
 const inputText = readFileSync('./input.txt', 'utf-8');
 const sectionsData = inputText.split('\n');
 
+type SectionName = 'firstSection' | 'secondSection';
+type SectionsDataArray = Record<SectionName, [number, number]>[];
+
+// Change the sections data into an array of 2 number arrays
+const sectionNumbersData = sectionsData.reduce<SectionsDataArray>((sectionsDataArray, currentSectionData) => {
+	const [firstSection, secondSection] = currentSectionData.split(',') as [string, string];
+	return [
+		...sectionsDataArray,
+		{
+			firstSection: firstSection.split('-').map(Number) as [number, number],
+			secondSection: secondSection.split('-').map(Number) as [number, number],
+		},
+	];
+}, []);
+
 /**
  * Part 1 - In how many assignment pairs does one range fully contain the other?
  * Answer - 471
  */
 
-const numberOfOverlaps = sectionsData.reduce((total, sections) => {
-	const [firstSection, secondSection] = sections.split(',') as [string, string];
-
-	const [firstSectionLowerBound, firstSectionUpperBound] = firstSection.split('-').map(Number);
-	const [secondSectionLowerBound, secondSectionUpperBound] = secondSection.split('-').map(Number);
+const numberOfOverlaps = sectionNumbersData.reduce((total, { firstSection, secondSection }) => {
+	const [firstSectionLowerBound, firstSectionUpperBound] = firstSection;
+	const [secondSectionLowerBound, secondSectionUpperBound] = secondSection;
 
 	const isSecondSectionContained =
 		firstSectionLowerBound <= secondSectionLowerBound && firstSectionUpperBound >= secondSectionUpperBound;
@@ -35,19 +48,16 @@ console.log(numberOfOverlaps); // 471
  * Answer - 888
  */
 
-const numberOfOverlaps2 = sectionsData.reduce((total, sections) => {
-	const [firstSection, secondSection] = sections.split(',') as [string, string];
-
-	const [firstSectionLowerBound, firstSectionUpperBound] = firstSection.split('-').map(Number);
-	const [secondSectionLowerBound, secondSectionUpperBound] = secondSection.split('-').map(Number);
+const numberOfOverlaps2 = sectionNumbersData.reduce((total, { firstSection, secondSection }) => {
+	const [firstSectionLowerBound, firstSectionUpperBound] = firstSection;
+	const [secondSectionLowerBound, secondSectionUpperBound] = secondSection;
 
 	const isNoBoundOverlap =
 		firstSectionUpperBound < secondSectionLowerBound || firstSectionLowerBound > secondSectionUpperBound;
 
-	if (isNoBoundOverlap) {
-		return total;
-	}
-	return total + 1;
+	if (!isNoBoundOverlap) return total + 1;
+
+	return total;
 }, 0);
 
 console.log(numberOfOverlaps2); // 888
