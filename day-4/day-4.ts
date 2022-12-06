54e1c5d736909659bf4efacbdb3f72b8669183f4
Eliad Tuksher <eliad91@gmail.com>
2022-12-06 20:22:27 +0200

    Solve day 4 part 1

--- day-4/day-4.ts
+++ day-4/day-4.ts
@@ -5,8 +5,25 @@
 import { readFileSync } from 'fs';
 
 const inputText = readFileSync('./input.txt', 'utf-8');
-const rucksackData = inputText.split('\n');
+const sectionsData = inputText.split('\n');
 
 /**
  * Part 1 - In how many assignment pairs does one range fully contain the other?
  */
+
+const numberOfOverlaps = sectionsData.reduce((total, sections) => {
+	const [firstSection, secondSection] = sections.split(',') as [string, string];
+
+	const [firstSectionLowerBound, firstSectionUpperBound] = firstSection.split('-').map(Number);
+	const [secondSectionLowerBound, secondSectionUpperBound] = secondSection.split('-').map(Number);
+
+	const isSecondSectionContained =
+		firstSectionLowerBound <= secondSectionLowerBound && firstSectionUpperBound >= secondSectionUpperBound;
+	const isFirstSectionContained =
+		firstSectionLowerBound >= secondSectionLowerBound && firstSectionUpperBound <= secondSectionUpperBound;
+
+	if (isFirstSectionContained || isSecondSectionContained) return total + 1;
+
+	return total;
+}, 0);
+console.log(numberOfOverlaps);
