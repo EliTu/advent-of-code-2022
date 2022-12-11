cad9fa04ed0ffb639d354720fa1a17f410d51dbd
Eliad Tuksher <eliad91@gmail.com>
2022-12-11 15:31:47 +0200

    Day 5 - looping over the input and updating the stacks

--- day-5/day-5.ts
+++ day-5/day-5.ts
@@ -2,6 +2,8 @@
  * https://adventofcode.com/2022/day/5
  */
 
+import { readFileSync } from 'fs';
+
 // Stack layout from the input data:
 // [M] [H]         [N]
 // [S] [W]         [F]     [W] [V]
@@ -13,7 +15,7 @@
 // [R] [P] [W] [N] [M] [P] [R] [Q] [L]
 //  1   2   3   4   5   6   7   8   9
 
-const stacks = {
+const stacks: Record<number, string[]> = {
 	1: ['R', 'N', 'F', 'V', 'L', 'J', 'S', 'M'],
 	2: ['P', 'N', 'D', 'Z', 'F', 'J', 'W', 'H'],
 	3: ['W', 'R', 'C', 'D', 'G'],
@@ -24,3 +26,25 @@ const stacks = {
 	8: ['Q', 'T', 'H', 'F', 'N', 'B', 'V'],
 	9: ['L', 'M', 'H', 'Z', 'N', 'F'],
 };
+
+console.log('initstacks', stacks);
+
+const inputText = readFileSync('./input.txt', 'utf-8');
+const instructions = inputText.split('\n');
+
+for (let i = 0; i < 4; i++) {
+	const instruction = instructions[i];
+	const numbersData = instruction.replace(/[a-z]/gi, '');
+
+	const [moveQuant, fromStack, toStack] = numbersData.split(' ').filter(Boolean).map(Number);
+
+	for (let j = 0; j < moveQuant; j++) {
+		const movedCrate = stacks[fromStack].pop();
+
+		if (movedCrate) {
+			stacks[toStack].push(movedCrate);
+		}
+	}
+}
+
+console.log('after', stacks);
