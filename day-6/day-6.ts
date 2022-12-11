17d28b8e5da9ca9b8a1c439a7d399b74b1028960
Eliad Tuksher <eliad91@gmail.com>
2022-12-11 17:41:59 +0200

    Day 6 - solve part 1

--- day-6/day-6.ts
+++ day-6/day-6.ts
@@ -7,6 +7,31 @@ import { readFileSync } from 'fs';
 const inputText = readFileSync('./input.txt', 'utf-8');
 const [signal] = inputText.split('\n');
 
-for (let i = 0; i < 10; i++) {
-	const set = new Set(...[signal[i], signal[i + 1], signal[i + 2], signal[i + 3]]);
+/**
+ * Part 1 - How many characters need to be processed before the first start-of-packet marker is detected?
+ * Answer: 1896
+ */
+
+let marker = 1;
+for (let i = 0; i < signal.length; i++) {
+	const isUnique = isCharSequenceUnique([signal[i], signal[i + 1], signal[i + 2], signal[i + 3]]);
+	if (isUnique) {
+		marker += i + 3;
+		break;
+	}
+}
+
+console.log(marker); // 1896
+
+/* Helpers */
+
+function isCharSequenceUnique(chars: string[]) {
+	const record: Record<string, string> = {};
+	for (let char of chars) {
+		if (record[char]) {
+			return false;
+		}
+		record[char] = char;
+	}
+	return true;
 }
