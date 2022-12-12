609daac96d6055a56fbab11bd9c6c30f0665e457
Eliad Tuksher <eliad91@gmail.com>
2022-12-12 23:49:05 +0200

    Day 8 part 1 - wip

--- day-8/day-8.ts
+++ day-8/day-8.ts
@@ -9,25 +9,23 @@ const treesMap = inputText.split('\n');
 
 let visibleTreeCounter = treesMap[0].length * 2 + treesMap.length * 2 - 4;
 
-for (let i = 1; i < treesMap.slice(0, 2).length - 1; i++) {
+for (let i = 1; i < treesMap.length - 1; i++) {
 	const currentTreesRow = treesMap[i];
 
-	console.log(treesMap[i], treesMap.slice(0, 1).length - 1);
+	for (let j = 1; j < currentTreesRow.length - 2; j++) {
+		const currentTree = currentTreesRow[j];
 
-	for (let j = 1; j < currentTreesRow.length - 1; j++) {
-		const currentTree = Number(currentTreesRow[j]);
+		if (currentTree === '0') continue;
 
-		if (currentTree === 0) continue;
+		const leftTree = currentTreesRow[j - 1];
+		const rightTree = currentTreesRow[j + 1];
+		const topTree = treesMap[i - 1][j];
+		const bottomTree = treesMap[i + 1][j];
 
-		const leftTree = Number(currentTreesRow[j - 1]);
-		const rightTree = Number(currentTreesRow[j + 1]);
-		const topTree = Number(treesMap[i - 1][j]);
-		const bottomTree = Number(treesMap?.[i + 1]?.[j] || 0);
-
-		if ([leftTree, rightTree, topTree, bottomTree].some((tree) => currentTree > tree)) {
+		if ([leftTree, rightTree, topTree, bottomTree].some((tree) => Number(currentTree) > Number(tree))) {
 			visibleTreeCounter += 1;
 		}
 	}
 }
 
-// console.log(visibleTreeCounter);
+console.log(visibleTreeCounter);
